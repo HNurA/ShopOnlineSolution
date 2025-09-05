9cc0c73de9c3bcf70e54f8db5deb677feafce019
Hilal Nur Albayrak <hnuralbayrak@gmail.com>
2025-09-05 09:31:04 +0300

    part 2 finished

---
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -0,0 +1,28 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Extensions
+{
+    public static class DtoConversions
+    {
+        // Bu method, db den gelen Product ve ProductCategory entitylerini alıp frontend e gönderilecek ProductDto objelerine dönüştürür.
+        // Her ürünün kategori adını da içerecek şekilde join yapar.
+        public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products, IEnumerable<ProductCategory> productCategories)
+        {
+            return (from product in products
+                    join productCategory in productCategories
+                    on product.CategoryId equals productCategory.Id
+                    select new ProductDto
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Description = product.Description,
+                        ImageURL = product.ImageURL,
+                        Price = product.Price,
+                        Qty = product.Qty,
+                        CategoryId = product.CategoryId,
+                        CategoryName = productCategory.Name, //join'den gelen kategori adı
+                    }).ToList();
+        }
+    }
+}
--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+using ShopOnline.Api.Repositories.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// Swagger is an interface description language for describing restful apis expressed using json
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+    );
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
