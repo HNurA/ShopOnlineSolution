03eefe1ebe9206f7daf7b1bcb8946910e1fa6213
Hilal Nur Albayrak <hnuralbayrak@gmail.com>
2025-09-05 14:50:32 +0300

    built home page and fetch data

--- ShopOnline.Web/Components/Pages/ProductsBase.cs
+++ ShopOnline.Web/Components/Pages/ProductsBase.cs
@@ -2,14 +2,14 @@
 using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 
-namespace ShopOnline.Web.Pages
+namespace ShopOnline.Web.Components.Pages
 {
-    public class ProductsBase:ComponentBase
+    public class ProductsBase : ComponentBase
     {
         [Inject]
-        public IProductService ProductService { get; set; }
+        public IProductService ProductService { get; set; } = default!;
 
-        public IEnumerable<ProductDto> Products { get; set; }
+        public IEnumerable<ProductDto> Products { get; set; } = new List<ProductDto>();
 
         protected override async Task OnInitializedAsync()
         {
